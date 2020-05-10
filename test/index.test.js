926053316aa1cba0a6d86fcb5fb62eae65f7b5e1
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-05-10 18:18:06 +0100

    write unit tests

---
+++ test/index.test.js
@@ -0,0 +1,27 @@
+const { getUrlFromLocation, getTimeFromTimeStamp } = require("../src");
+const chai = require("chai");
+
+const { expect } = chai;
+describe("Weather", () => {
+  it("tests the getUrlFromLocation function", (done) => {
+    const locations = ["New_York", 10005];
+
+    const expectedResponse = [
+      {
+        name: "New_York",
+        url:
+          "https://api.openweathermap.org/data/2.5/weather?q=New_York&appid=8712313176531723517631&units=metric",
+      },
+      {
+        name: 10005,
+        url:
+          "https://api.openweathermap.org/data/2.5/weather?zip=10005&appid=8712313176531723517631&units=metric",
+      },
+    ];
+    console.log(getUrlFromLocation(locations));
+    expect(getUrlFromLocation(locations).length).to.equals(2);
+    // expect(getUrlFromLocation(locations)).to.equals(expectedResponse);
+    done();
+    // });
+  });
+});
